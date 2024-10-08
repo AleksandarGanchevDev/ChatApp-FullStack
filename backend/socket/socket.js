face85388c37959717fa605c0dcd5f5f81269ce3
DefinitelyNotSasho <sashogan4ev@gmail.com>
2024-10-08 19:29:54 +0300

    Changed colors from signup page

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: [
+      "https://chat-app-full-stack-kqmd-aleksandars-projects-13f4ec34.vercel.app",
+    ],
     methods: ["GET", "POST"],
   },
 });
