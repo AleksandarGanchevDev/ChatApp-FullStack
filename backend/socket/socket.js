04feea1dd9e1c6964c7b8b140d1289052e4efaa5
DefinitelyNotSasho <sashogan4ev@gmail.com>
2024-10-08 19:31:46 +0300

    Link changed from backend socket

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,9 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [
-      "https://chat-app-full-stack-kqmd-aleksandars-projects-13f4ec34.vercel.app",
-    ],
+    origin: ["https://chat-app-full-stack-kqmd.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
